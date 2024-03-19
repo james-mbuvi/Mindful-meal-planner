dc8c42f84576ba8ff98194961fd96f8a8bbc2d5f
Alex Kimeu <alexkimeu1999@gmail.com>
2024-03-19 10:40:27 +0300

    Edited homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,36 +1,50 @@
 import { LuSalad } from "react-icons/lu";
 import { TbTargetArrow } from "react-icons/tb";
-import { BiSolidSearchAlt2 } from "react-icons/bi";
+import { RiSearch2Line } from "react-icons/ri";
 import home from "../../assets/home.jpg"
 
 export default function Homepage() {
     return (
-        <div className="px-14 ">
-            <div className="flex">
-                <div className="flex flex-col">
-                    <h1 className="text-[40px]">From chaotic kitchen to culinary Bliss:</h1>
-                    <p className="text-[25px]">Let our Meal Planner Revolutionize your<br />Routine!</p>
-                    <button className="text-[18px] bg-pickle text-white px-">Get Started</button>
+        <div className="px-[120px]">
+            <div className="flex justify-between font-sans">
+                <div className="flex flex-col justify-center">
+                    <h1 className="text-[35px] my-2 font-bold">From chaotic kitchen to culinary Bliss:</h1>
+                    <h1 className="text-[30px] font-semibold">Let our Meal Planner Revolutionize your<br />Routine!</h1>
+                    <p className="text-[18px] text-center mt-3 font-medium">Click to get started! to set your meal preferences</p>
+                    <div className="flex justify-center mt-5">
+                        <button
+                            className="text-[18px] bg-pickle hover:bg-pickle-lg text-white py-[4px] px-[8px] w-[250px] rounded-[30px] h-[55px] font-normal">
+                            Get Started
+                        </button>
+                    </div>
                 </div>
-                <img src={ home } alt="Bg"  className="w-[400px] h-[400px] rounded-lg"/>
+                <img src={home} alt="Bg" className="w-[500px] h-[450px] rounded-lg" />
             </div>
-            <div className="flex flex-row h-">
-                <div className="">
-                    <LuSalad className=""/>
-                    <p>Nutritional Convenience</p>
-                    <p>Enjoy the ease of planning balanced meals tailored to your nutritional needs...</p>
+            <div className="flex flex-row h-[135px] w-auto bg-pickle justify-between rounded-[10px] my-[50px] p-3">
+                <div className="flex gap-[10px]">
+                    <div>
+                        <LuSalad className="w-[80px] h-[80px]" />
+                        </div>
+                    <div>
+                        <p className="text-center font-medium text-[18px]">Nutritional Convenience</p>
+                        <p className="text-white">Enjoy the ease of planning balanced meals tailored to your nutritional needs...</p>
+                    </div>
                 </div>
-                <div className="">
-                    <TbTargetArrow />
-                    <p>Personalized Guidance</p>
-                    <p>Receive personalized meal recommendations and expert guidance...</p>
+                <div className="flex gap-[10px]">
+                    <TbTargetArrow className="w-[80px] h-[80px]"/>
+                    <div>
+                        <p className="text-center font-medium text-[18px]">Personalized Guidance</p>
+                        <p className="text-white">Receive personalized meal recommendations and expert guidance...</p>
+                    </div>
                 </div>
-                <div className="">
-                    <BiSolidSearchAlt2 />
-                    <p>Culinary Inspiration</p>
-                    <p>Unleash your creativity in the kitchen with a wealth of delicious recipes...</p>
+                <div className="flex gap-[10px]">
+                    <RiSearch2Line className="w-[80px] h-[80px]"/>
+                    <div>
+                        <p className="text-center font-medium text-[18px]">Culinary Inspiration</p>
+                        <p className="text-white">Unleash your creativity in the kitchen with a wealth of delicious recipes...</p>
+                    </div>
                 </div>
             </div>
         </div>
     )
-};
+}
