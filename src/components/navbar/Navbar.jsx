e7787fc338e53ca2dbebc12468be2a74b79407ce
Alex Kimeu <alexkimeu1999@gmail.com>
2024-03-19 22:19:44 +0300

    Refactored navbar links code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,6 @@ import { useState, useEffect } from "react";
 import logo from "../../assets/logo.png";
 import darkmode from "../../assets/darkmode.png";
 
-const navigation = [
-  { name: "Home", href: "/homepage", current: false },
-  { name: "My Meal Plan", href: "MyMealPlan", current: false },
-];
 // set dark theme
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -55,20 +51,24 @@ export const Navbar = () => {
               </div>
               <div className="hidden sm:ml-6 sm:block  ">
                 <div className="flex space-x-4 mt-4 ">
-                  {/* navigation area */}
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      className={classNames(
-                        "text-black hover:bg-stone-200 hover:text-black dark:hover:text-black dark:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
+                  <Link
+                    to="/homepage"
+                    className={classNames(
+                      "text-black hover:bg-stone-200 hover:text-black dark:hover:text-black dark:text-white",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/MyMealPlan"
+                    className={classNames(
+                      "text-black hover:bg-stone-200 hover:text-black dark:hover:text-black dark:text-white",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                  >
+                    My Meal Plan
+                  </Link>
                 </div>
               </div>
 
@@ -99,20 +99,24 @@ export const Navbar = () => {
           {/* navbar when in a sm screen */}
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    "text-black hover:bg- hover:text-",
-                    "block rounded-md px-3 py-2 text-base font-medium dark:text-white"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+                <Link
+                to="/homepage"
+                className={classNames(
+                  "text-black hover:bg- hover:text-",
+                  "block rounded-md px-3 py-2 text-base font-medium dark:text-white"
+                )}
+              >
+                Home
+              </Link>
+              <Link
+                to="/MyMealPlan"
+                className={classNames(
+                  "text-black hover:bg- hover:text-",
+                  "block rounded-md px-3 py-2 text-base font-medium dark:text-white"
+                )}
+              >
+                My Meal Plan
+              </Link>                             
             </div>
           </Disclosure.Panel>
         </>
