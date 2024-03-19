aef432053409866a701f617038bac5f556808c77
Alex Kimeu <alexkimeu1999@gmail.com>
2024-03-19 23:08:25 +0300

    Added functionality to the get started button

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { LuSalad } from "react-icons/lu";
 import { TbTargetArrow } from "react-icons/tb";
 import { RiSearch2Line } from "react-icons/ri";
 import home from "../../assets/home.jpg"
 import { ResponsiveText } from "./ResponsiveText";
+import { Preferences } from "../preferences/Preferences";
+
+
 
 export const Homepage = () => {
+    const [showPreferences, setShowPreferences] = useState(false);
+
+    if (showPreferences) {
+        return <Preferences onClose={() => setShowPreferences(false)} />;
+    }
+
     return (
         <div className="px-6 md:px-12 lg:px-[120px] h-screen flex flex-col bg-white dark:bg-dm">
             <div className="flex flex-col md:flex-row justify-evenly font-sans">
@@ -14,9 +24,15 @@ export const Homepage = () => {
                     <p className="text-lg text-center font-medium text-dm dark:text-white">Click to get started! to set your meal preferences</p>
                     <div className="flex justify-center mt-5">
                         <button
-                            className="text-lg bg-pickle hover:bg-pickle-lg text-white py-2 px-4 w-48 rounded-lg h-12 font-normal transition-colors duration-200 ease-in-out">
+                            className="text-lg bg-pickle hover:bg-pickle-lg text-white py-2 px-4 w-48 rounded-lg h-12 font-normal transition-colors duration-200 ease-in-out"
+                            onClick={() => setShowPreferences(true)}
+                        >
                             Get Started
                         </button>
+
+                        {showPreferences && (
+                            <Preferences onClose={() => setShowPreferences(false)} />
+                        )}
                     </div>
                 </div>
                 <img src={home} alt="Bg" className="hidden md:block md:w-96 md:h-80 lg:w-[500px] lg:h-[500px] rounded-lg mt-6 md:mt-0" />
@@ -28,7 +44,7 @@ export const Homepage = () => {
                     </div>
                     <div>
                         <p className="text-center font-medium text-lg">Nutritional Convenience</p>
-                        <ResponsiveText 
+                        <ResponsiveText
                             fullText="Enjoy the ease of planning balanced meals tailored to your nutritional needs, ensuring you stay on track with your health goals without sacrificing convenience."
                             shortText="Enjoy the ease of planning balanced meals..."
                         />
@@ -36,11 +52,11 @@ export const Homepage = () => {
                 </div>
                 <div className="flex flex-col md:flex-row gap-2 md:gap-4">
                     <div className="flex justify-center md:justify-center lg:justify-start">
-                        <TbTargetArrow className="w-16 h-16 md:w-20 md:h-20"/>
+                        <TbTargetArrow className="w-16 h-16 md:w-20 md:h-20" />
                     </div>
                     <div>
                         <p className="text-center font-medium text-lg">Personalized Guidance</p>
-                        <ResponsiveText 
+                        <ResponsiveText
                             fullText="Receive personalized meal recommendations and expert guidance, empowering you to make healthier choices that fit your unique lifestyle and preferences."
                             shortText="Receive personalized meal ideas..."
                         />
@@ -48,11 +64,11 @@ export const Homepage = () => {
                 </div>
                 <div className="flex flex-col md:flex-row gap-2 md:gap-4">
                     <div className="flex justify-center md:justify-center lg:justify-start">
-                        <RiSearch2Line className="w-16 h-16 md:w-20 md:h-20"/>
+                        <RiSearch2Line className="w-16 h-16 md:w-20 md:h-20" />
                     </div>
                     <div>
                         <p className="text-center font-medium text-lg">Culinary Inspiration</p>
-                        <ResponsiveText 
+                        <ResponsiveText
                             fullText="Unleash your creativity in the kitchen with a wealth of delicious recipes and culinary inspiration, making mealtime a delightful adventure filled with flavor and excitement."
                             shortText="Unleash your creativity in the kitchen..."
                         />
