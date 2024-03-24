f5b64a8ad3690d37286f80920dacb6d920b3bedf
Ndisyamwende <sharonndisya2002@gmail.com>
2024-03-24 13:30:15 +0300

    completed recipe page

--- src/pages/recipes/Recipe.jsx
+++ src/pages/recipes/Recipe.jsx
@@ -23,30 +23,36 @@ export const Recipe = () => {
   }, [id]);
 
   if (!recipe) {
-    return <div>Loading...</div>;
+    return <div className="flex justify-center items-center h-screen">Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.image} alt={recipe.title} />
-      <h2>Ingredients:</h2>
-      <ol className="list-decimal list-inside">
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ol>
-      <h2>Recipe:</h2>
-      <ol className="list-decimal list-inside">
-        {recipe.recipe.map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
-      </ol>
-      <h2>Nutritional Values</h2>
-      <p>Calories: {recipe.nutrition.Calories}</p>
-      <p>Fat: {recipe.nutrition.Fat}</p>
-      <p>Carbs: {recipe.nutrition.Carbs}</p>
-      <p>Protein: {recipe.nutrition.Protein}</p>
+    <div className="dark:bg-dm text-dm dark:text-white">
+      <div className="lg:w-1/2">
+        <img src={recipe.image} alt={recipe.title} className="w-full" />
+      </div>
+      <div className="lg:w-1/2 p-6 bg-gray-100 dark:bg-gray-800">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800 dark:text-gray-200">{recipe.title}</h1>
+        <h2 className="text-xl font-bold mb-2">Ingredients:</h2>
+        <ul className="list-disc list-inside mb-4">
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+        <h2 className="text-xl font-bold mb-2">Recipe:</h2>
+        <ol className="list-decimal list-inside mb-4">
+          {recipe.recipe.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ol>
+        <h2 className="text-xl font-bold mb-2">Nutritional Values</h2>
+        <div>
+          <p><span className="font-bold">Calories:</span> {recipe.nutrition.Calories}</p>
+          <p><span className="font-bold">Fat:</span> {recipe.nutrition.Fat}</p>
+          <p><span className="font-bold">Carbs:</span> {recipe.nutrition.Carbs}</p>
+          <p><span className="font-bold">Protein:</span> {recipe.nutrition.Protein}</p>
+        </div>
+      </div>
     </div>
   );
 };
